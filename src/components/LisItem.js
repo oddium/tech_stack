6737c3ef18b164e5576aa19a0011d5367a716a46
Mehmet Tenekeci <info@mehmettenekeci.com>
2019-01-03 01:50:47 +0300

    some improvement 2

--- src/components/LisItem.js
+++ src/components/LisItem.js
@@ -1,20 +1,50 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableWithoutFeedback, View} from 'react-native';
+import {connect} from 'react-redux';
 import {CardSection} from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+    renderDescription() {
+
+        const {library, expanded} = this.props;
+
+        if (expanded) {
+            return (
+                <Text>{library.item.description}</Text>
+            );
+        }
+    }
+
     render() {
 
         const {id, title} = this.props.library.item;
         const {titleStyle} = styles;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>{title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
+
+const mapStateToProps = (state, ownProps) => {
+
+    const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+    return {expanded};
+};
 
 const styles = {
     titleStyle: {
@@ -23,4 +53,4 @@ const styles = {
     }
 };
 
-export default ListItem;
+export default connect(mapStateToProps, actions)(ListItem);
